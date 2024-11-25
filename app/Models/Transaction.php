e92656ad67f4ee2073d448b499b8d587193617d0
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-26 00:30:14 +0200

    create client and transaction models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'client_id',
+        'amount'
+    ];
+
+    public function client(): HasOne
+    {
+        return $this->hasOne(Client::class, 'id', 'client_id');
+    }
+}
