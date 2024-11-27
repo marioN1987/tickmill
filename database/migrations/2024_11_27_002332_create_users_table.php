d9879567046b8712ff43bee187caa80df54bcd4d
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-27 02:41:33 +0200

    client, user migrations

--- database/migrations/2024_11_27_002332_create_users_table.php
+++ database/migrations/2024_11_27_002332_create_users_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('users');
     }
 };
