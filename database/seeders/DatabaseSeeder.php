c9b405d513ecca60dcf0d5ea07fc1f22f175e443
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-27 02:41:44 +0200

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Client;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,13 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([ 
             'firstname' => 'admin',
             'lastname' => 'admin',
-            'avatar' => 'public/storage',
-            // 'email_verified_at' => now(),
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'email' => 'admin@admin.com',
         ]);
 
-        User::factory(50)->create();
-        Transaction::factory(50)->create();
+        // Client::factory(50)->create();
+        // Transaction::factory(50)->create();
     }
 }
