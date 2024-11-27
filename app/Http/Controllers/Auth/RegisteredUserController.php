432ac5ac16b8c04b40dce14d479fd54ca2e7cb4c
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-27 02:39:34 +0200

    register client be logic

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Client;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class RegisteredUserController extends Controller
 {
@@ -30,22 +32,38 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        //proper custom validation messages
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.Client::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ],
+        [
+            'avatar.required' => 'Please select avatar!',
+            'avatar.uploaded' => 'Failed to upload an image. The image maximum size is 2MB.'
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
+        $image = $request->file('avatar');
+        $date = Carbon::now()->format('Y-m-d');
+        $filename = $date.'/'.$request->avatar->getClientOriginalName();
+
+        //save image in db with current date
+        $image->storeAs('avatars', $filename, 'public');
+
+        $user = Client::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'avatar' => $filename,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+        //Auth::login($user);
+        return redirect(route('welcome'));
+       // return redirect(route('dashboard', absolute: false));
     }
 }
