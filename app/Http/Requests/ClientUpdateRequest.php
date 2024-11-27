a1a1733a799ca07791cbc5d5229ca15c50b3fce7
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-27 02:39:52 +0200

    client validatioin form request

---
+++ app/Http/Requests/ClientUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Client;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ClientUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|integer|exists:'.Client::class.',id',
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(Client::class)->ignore($this->user()->id),
+            ]
+        ];
+    }
+}
