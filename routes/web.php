b500a67572c824d019a8de8fdf864e00c841db16
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-27 02:43:02 +0200

    added routes in web php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ClientController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -12,16 +12,17 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/list', [ClientController::class, 'list'])->name('clients.list');
+    Route::get('/edit', [ClientController::class, 'edit'])->name('clients.edit');
+    Route::post('/update', [ClientController::class, 'update'])->name('clients.update');
+    Route::delete('/delete', [ClientController::class, 'delete'])->name('clients.delete');
 });
 
 require __DIR__.'/auth.php';
