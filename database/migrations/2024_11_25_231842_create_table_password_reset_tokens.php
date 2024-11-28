11e235229d0f9351821890c8a6e49bab0657e1f1
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-29 00:12:49 +0200

    fix table name

--- database/migrations/2024_11_25_231842_create_table_password_reset_tokens.php
+++ database/migrations/2024_11_25_231842_create_table_password_reset_tokens.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_password_reset_tokens');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
