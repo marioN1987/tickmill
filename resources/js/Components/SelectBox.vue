df82c442e44580349b62451e276aa67101ca96e3
Marios Nikolaou <marios.nikolaou@chrisons.com>
2024-11-30 00:17:30 +0200

    select box vue created

---
+++ resources/js/Components/SelectBox.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    options: Array
+})
+</script>
+
+<template>
+    <select>
+        <option value="">Please select ...</option>
+        <option v-for="option in options" :key="option.id" :value="option.id">
+            {{ option.firstname + ' ' + option.lastname + ' | ' + option.email }}
+        </option>
+    </select>
+</template>
